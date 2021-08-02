71404d57451e9fffb059e165b4a619e65d86b81d
mattfisher <nuggsrocks@yahoo.com>
2021-08-02 19:16:06 -0400

    Rect move method

--- __tests__/Rect.test.js
+++ __tests__/Rect.test.js
@@ -1,5 +1,21 @@
-describe('a', () => {
-  it('should equal a', () => {
-    expect('a').toEqual('a')
+import { Rect } from '../src/js/Rect'
+
+describe('Rect', () => {
+  describe('move(deltaX, deltaY)', () => {
+    describe('should change x and y values by given delta', () => {
+      it.each([
+        {deltaX: 10, deltaY: 10, expected: {x: 10, y: 10}},
+        {deltaX: 100, deltaY: -10, expected: {x: 100, y: -10}},
+        {deltaX: -10, deltaY: 100, expected: {x: -10, y: 100}},
+        {deltaX: 10, deltaY: -500, expected: {x: 10, y: -500}}
+      ])('Rect.move($deltaX, $deltaY)', ({deltaX, deltaY, expected}) => {
+        const rect = new Rect()
+
+        rect.move(deltaX, deltaY)
+
+        expect(rect).toHaveProperty('x', expected.x)
+        expect(rect).toHaveProperty('y', expected.y)
+      })
+    })
   })
 })
--- src/js/Rect.js
+++ src/js/Rect.js
@@ -1,1 +1,11 @@
-export const Rect = () => {}
+export class Rect {
+  constructor () {
+    this.x = 0
+    this.y = 0
+  }
+
+  move (deltaX, deltaY) {
+    this.x += deltaX
+    this.y += deltaY
+  }
+}
