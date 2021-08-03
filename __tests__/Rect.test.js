e102d835736caf822d98dd2e178eed9982674c7c
mattfisher <nuggsrocks@yahoo.com>
2021-08-03 18:22:08 -0400

    rect logic

--- __tests__/Rect.test.js
+++ __tests__/Rect.test.js
@@ -18,4 +18,54 @@ describe('Rect', () => {
       })
     })
   })
+
+  describe('hasCollidedWithBorder(canvas)', () => {
+    describe('should return false if there is no collision', () => {
+      it.each([
+        {rect: {x: 0, y: 0, size: 10}, canvas: {width: 100, height: 100}},
+        {rect: {x: 90, y: 90, size: 10}, canvas: {width: 100, height: 100}}
+      ])('$rect and $canvas should return false', ({rect, canvas}) => {
+        expect(new Rect(rect).hasCollidedWithBorder(canvas)).toEqual(false)
+      })
+    })
+
+    describe('should return border(s) that rect has collided with if there is a collision', () => {
+      it.each([
+        {rect: {x: -10, y: 0, size: 10}, canvas: {width: 100, height: 100}, expected: 'left'},
+        {rect: {x: 100, y: 0, size: 10}, canvas: {width: 100, height: 100}, expected: 'right'},
+        {rect: {x: 0, y: -10, size: 10}, canvas: {width: 100, height: 100}, expected: 'top'},
+        {rect: {x: 0, y: 100, size: 10}, canvas: {width: 100, height: 100}, expected: 'bottom'},
+        {rect: {x: -10, y: -10, size: 10}, canvas: {width: 100, height: 100}, expected: ['left', 'top']},
+        {rect: {x: -10, y: 100, size: 10}, canvas: {width: 100, height: 100}, expected: ['left', 'bottom']},
+        {rect: {x: 100, y: -10, size: 10}, canvas: {width: 100, height: 100}, expected: ['right', 'top']},
+        {rect: {x: 100, y: 100, size: 10}, canvas: {width: 100, height: 100}, expected: ['right', 'bottom']},
+      ])('$rect and $canvas should return $expected', ({rect, canvas, expected}) => {
+        expect(new Rect(rect).hasCollidedWithBorder(canvas)).toEqual(expected)
+      })
+    })
+  })
+
+  describe('hasCollidedWithRect(rect)', () => {
+    describe('should return false if there is no overlap', () => {
+      it.each([
+        {rect1: {x: 0, y: 0, size: 10}, rect2: {x: 10, y: 10, size: 10}},
+        {rect1: {x: 90, y: 90, size: 10}, rect2: {x: 85, y: 80, size: 10}},
+        {rect1: {x: 10, y: 0, size: 10}, rect2: {x: 0, y: 0, size: 10}},
+        {rect1: {x: 0, y: 0, size: 100}, rect2: {x: 0, y: 101, size: 10}}
+      ])('$rect1 and $rect2 should return false', ({rect1, rect2}) => {
+        expect(new Rect(rect1).hasCollidedWithRect(new Rect(rect2))).toEqual(false)
+      })
+    })
+
+    describe('should return true if there is overlap', () => {
+      it.each([
+        {rect1: {x: 0, y: 0, size: 10}, rect2: {x: 5, y: 9.9, size: 10}},
+        {rect1: {x: 90, y: 90, size: 10}, rect2: {x: 95, y: 95, size: 10}},
+        {rect1: {x: 10, y: 0, size: 10}, rect2: {x: 5, y: 5, size: 10}},
+        {rect1: {x: 0, y: 0, size: 100}, rect2: {x: 99, y: 99, size: 10}}
+      ])('$rect1 and $rect2 should return true', ({rect1, rect2}) => {
+        expect(new Rect(rect1).hasCollidedWithRect(new Rect(rect2))).toEqual(true)
+      })
+    })
+  })
 })
--- src/js/Rect.js
+++ src/js/Rect.js
@@ -1,11 +1,61 @@
 export class Rect {
-  constructor () {
-    this.x = 0
-    this.y = 0
+  constructor ({x = 0, y = 0, size = 0} = {}) {
+    this.x = x
+    this.y = y
+    this.size = size
   }
 
   move (deltaX, deltaY) {
     this.x += deltaX
     this.y += deltaY
+  }
+
+  hasCollidedWithBorder(canvas) {
+    let collidedBorder = false
+
+    if (this.x < 0) {
+      collidedBorder = 'left'
+    }
+
+    if (this.x > canvas.width - this.size) {
+      collidedBorder = 'right'
+    }
+
+    if (this.y < 0) {
+      if (collidedBorder !== false) {
+        collidedBorder = [collidedBorder, 'top']
+      } else {
+        collidedBorder = 'top'
+      }
+    }
+
+    if (this.y > canvas.width - this.size) {
+      if (collidedBorder !== false) {
+        collidedBorder = [collidedBorder, 'bottom']
+      } else {
+        collidedBorder = 'bottom'
+      }
+    }
+
+    return collidedBorder
+  }
+
+  hasCollidedWithRect(rect) {
+    const rectPoints = [
+      {x: rect.x, y: rect.y},
+      {x: rect.x + rect.size, y: rect.y},
+      {x: rect.x, y: rect.y + rect.size},
+      {x: rect.x + rect.size, y: rect.y + rect.size}
+    ]
+
+    for (const point of rectPoints) {
+      if (point.x > this.x && point.x < this.x + this.size && point.y > this.y && point.y < this.y + this.size) {
+        return true
+      }
+    }
+
+
+
+    return false
   }
 }
